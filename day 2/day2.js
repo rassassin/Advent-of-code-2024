f0742c8ff4a80b242232e9312978d5f25d03493c
Reece Thandi <reecethandi11@gmail.com>
2024-12-13 19:26:09 +0000

    further parsed each report into its own array

--- day 2/day2.js
+++ day 2/day2.js
@@ -2,11 +2,29 @@ const fs = require("fs");
 const input = fs.readFileSync("./day2input.txt", "utf-8").split("/\r?\n/");
 
 function parseInput(input) {
-  let listOfLevels = [];
+  let listOfLevelsAsStrings = [];
+  let listOfLevelsAsArrayOfStrings = [];
   for (const line of input) {
-    listOfLevels = line.split(/\r?\n+/);
+    listOfLevelsAsStrings = line.split(/\r?\n+/);
   }
-  return listOfLevels;
+  for (let i = 0; i < listOfLevelsAsStrings.length; i++) {
+    listOfLevelsAsArrayOfStrings[i] = [listOfLevelsAsStrings[i]];
+  }
+  return listOfLevelsAsArrayOfStrings;
 }
 
-console.log(parseInput(input));
+const getSafeLevelsCount = (parsedReports) => {
+  // for (const report of parsedReports) {
+  //   console.log(report);
+  //   for (const level of report) {
+  //   }
+  // }
+};
+
+function solveDayTwo(input) {
+  const parsedReports = parseInput(input);
+  console.log(parsedReports);
+  const numberOfSafeLevels = getSafeLevelsCount(parsedReports);
+}
+
+solveDayTwo(input);
