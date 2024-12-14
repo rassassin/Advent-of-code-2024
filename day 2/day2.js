7ef4da307319357bb8d2d00618ee531441d872ce
Reece Thandi <reecethandi11@gmail.com>
2024-12-14 15:17:21 +0000

    saved old code to compare

--- day 2/day2.js
+++ day 2/day2.js
@@ -13,17 +13,6 @@ function parseInput(input) {
   return [inputAsNumArray];
 }
 
-// function parseInput(input) {
-//   let listOfLevelsAsStrings = input.map((line) => line.split(/\r?\n+/));
-//   const listOfLevelsAsArrayOfStrings = listOfLevelsAsStrings.map((stringArray) => stringArray.map((str) => [str]));
-//   let newVar = [];
-//   for (const level of listOfLevelsAsArrayOfStrings[0][0]) {
-//     newVar = level.split(" ");
-//   }
-
-//   return listOfLevelsAsArrayOfStrings;
-// }
-
 const getSafeLevelsCount = (parsedReports) => {
   for (const report of parsedReports) {
     console.log(report[0][0]);
@@ -36,5 +25,37 @@ function solveDayTwo(input) {
 }
 
 solveDayTwo(input);
+
+// function parseInput(input) {
+//   let listOfLevelsAsStrings = [];
+//   for (const line of input) {
+//     listOfLevelsAsStrings = line.split(/\r?\n+/);
+//   }
+
+//   let listOfLevelsAsArrayOfStrings = [];
+//   for (let i = 0; i < listOfLevelsAsStrings.length; i++) {
+//     listOfLevelsAsArrayOfStrings[i] = [listOfLevelsAsStrings[i]];
+//   }
+
+//   let inputAsNumArray = [];
+//   for (const level of listOfLevelsAsArrayOfStrings) {
+//     for (const report of level) {
+//       const value = report.split(" ").map(Number);
+//       inputAsNumArray.push(value);
+//     }
+//   }
+//   return [inputAsNumArray];
+// }
+
+// function parseInput(input) {
+//   let listOfLevelsAsStrings = input.map((line) => line.split(/\r?\n+/));
+//   const listOfLevelsAsArrayOfStrings = listOfLevelsAsStrings.map((stringArray) => stringArray.map((str) => [str]));
+//   let newVar = [];
+//   for (const level of listOfLevelsAsArrayOfStrings[0][0]) {
+//     newVar = level.split(" ");
+//   }
+
+//   return listOfLevelsAsArrayOfStrings;
+// }
 
 // let listOfLevelsAsArrayOfStrings = listOfLevelsAsStrings.map((string, i) => (string[i] = [string[i]]));
