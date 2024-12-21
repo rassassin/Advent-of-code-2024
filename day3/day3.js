d2c0da49ff57b99b177e663d062262e93f66093d
Reece Thandi <reecethandi11@gmail.com>
2024-12-21 19:23:15 +0000

    Parsed input cleanly

--- day3/day3.js
+++ day3/day3.js
@@ -1,4 +1,14 @@
 const fs = require("fs");
 const input = fs.readFileSync("./day3input.txt", "utf-8").split("/\r?\n/");
 
-console.log(input);
+const parseInput = (input) =>
+  input[0]
+    .split(/['mul('','')']/)
+    .map((i) => Number(i))
+    .filter((elm) => elm);
+
+const solveDayThree = (input) => {
+  const getInputAsNumArray = parseInput(input);
+};
+
+console.log(parseInput(input));
