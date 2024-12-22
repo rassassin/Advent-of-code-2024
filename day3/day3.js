62840620cb8feee17e658e128e8f77579b9dc7c5
Reece Thandi <reecethandi11@gmail.com>
2024-12-22 20:18:01 +0000

    solved day 3 part 1

--- day3/day3.js
+++ day3/day3.js
@@ -1,16 +1,42 @@
 const fs = require("fs");
 const input = fs.readFileSync("./day3input.txt", "utf-8").split("/\r?\n/");
 
-const parseInput = (input) =>
-  input[0]
-    .split(/['mul('','')']/)
-    .map((i) => Number(i))
-    .filter((elm) => elm);
+function getOnlyNumbers(arrOfStrings) {
+  let onlyRelevantNumsAsStrings = [];
+  for (let i = 0; i < arrOfStrings.length; i++) {
+    let result = "";
+    for (let j = 0; j < arrOfStrings[i].length; j++) {
+      if (!isNaN(arrOfStrings[i][j]) && arrOfStrings[i][j + 1] === ")") {
+        result += arrOfStrings[i][j];
+        break;
+      }
+      result += arrOfStrings[i][j];
+    }
+    onlyRelevantNumsAsStrings.push(result);
+  }
+  return onlyRelevantNumsAsStrings;
+}
+const parseInput = (input) => {
+  const splitAllMulStrings = input[0].split("mul(");
+  const onlyNumbersAsStrings = getOnlyNumbers(splitAllMulStrings);
+  let arr = [];
+  for (let i = 0; i < onlyNumbersAsStrings.length; i++) {
+    let temp = onlyNumbersAsStrings[i]
+      .split(",")
+      .map((i) => Number(i))
+      .filter((elm) => elm);
+    if (temp.length == 2) arr.push(temp);
+  }
+  return arr.flat();
+};
 
 const solvePartOne = (numArray) => {
   let count = 0;
   for (let i = 0; i < numArray.length; i += 2) {
-    count += numArray[i] * numArray[i + 1];
+    console.log("Num1:", numArray[i]);
+    console.log("Num2:", numArray[i + 1]);
+    count = count + numArray[i] * numArray[i + 1];
+    // console.log(count);
   }
   return count;
 };
