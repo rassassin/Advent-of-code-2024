b318f99201cbfa9fd2d0137815547b6a798e5e34
Reece Thandi <reecethandi11@gmail.com>
2025-01-01 22:02:35 +0000

    WIP-Part one returns the incorrect number right now

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,51 @@
 const fs = require("fs");
 const input = fs.readFileSync("./day4input.txt", "utf-8").split("\n");
+const xDir = [-1, -1, -1, 0, 0, 1, 1, 1];
+const yDir = [-1, 0, 1, -1, 1, -1, 0, 1];
 
 const parseDataTo2DArray = (input) => input.map((row) => row.split(""));
 
+const search2DArray = (twoDimensionalArray, row, col, word) => {
+  if (twoDimensionalArray[row][col] !== word[0]) return false;
+
+  for (let dir = 0; dir < 8; dir++) {
+    let currX = row + xDir[dir];
+    let currY = col + yDir[dir];
+    let charToCheck = 1;
+
+    for (charToCheck = 1; charToCheck < word.length; charToCheck++) {
+      if (currX >= twoDimensionalArray.length || currX < 0 || currY >= twoDimensionalArray[0].length || currY < 0) break;
+
+      if (twoDimensionalArray[currX][currY] !== word[charToCheck]) break;
+
+      currX += xDir[dir];
+      currY += yDir[dir];
+    }
+
+    if (charToCheck === word.length) return true;
+  }
+
+  return false;
+};
+
+function searchForWord(twoDimensionalArray, word) {
+  let count = 0;
+
+  for (let i = 0; i < twoDimensionalArray.length; i++) {
+    for (let j = 0; j < twoDimensionalArray[0].length; j++) {
+      if (search2DArray(twoDimensionalArray, i, j, word)) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
 const solveDayFour = (input) => {
   const twoDimensionalArray = parseDataTo2DArray(input);
-  return twoDimensionalArray;
+  const findOccurencesOfXmas = searchForWord(twoDimensionalArray, "XMAS");
+  return findOccurencesOfXmas;
 };
 
 console.log(solveDayFour(input));
