f7bc211b0e845a19d01cfbe277aae9118fdfa3bb
Reece Thandi <reecethandi11@gmail.com>
2025-01-01 23:19:02 +0000

    gets correct answer with practice input but not with real input

--- day4/day4.js
+++ day4/day4.js
@@ -8,24 +8,24 @@ const parseDataTo2DArray = (input) => input.map((row) => row.split(""));
 const search2DArray = (twoDimensionalArray, row, col, word) => {
   if (twoDimensionalArray[row][col] !== word[0]) return false;
 
+  let count = 0;
+
   for (let dir = 0; dir < 8; dir++) {
     let currX = row + xDir[dir];
     let currY = col + yDir[dir];
     let charToCheck = 1;
 
     for (charToCheck = 1; charToCheck < word.length; charToCheck++) {
-      if (currX >= twoDimensionalArray.length || currX < 0 || currY >= twoDimensionalArray[0].length || currY < 0) break;
-
-      if (twoDimensionalArray[currX][currY] !== word[charToCheck]) break;
+      if (currX >= twoDimensionalArray.length || currX < 0 || currY >= twoDimensionalArray[0].length || currY < 0 || twoDimensionalArray[currX][currY] !== word[charToCheck]) break;
 
       currX += xDir[dir];
       currY += yDir[dir];
     }
 
-    if (charToCheck === word.length) return true;
+    if (charToCheck === word.length) count++;
   }
 
-  return false;
+  return count;
 };
 
 function searchForWord(twoDimensionalArray, word) {
@@ -33,9 +33,7 @@ function searchForWord(twoDimensionalArray, word) {
 
   for (let i = 0; i < twoDimensionalArray.length; i++) {
     for (let j = 0; j < twoDimensionalArray[0].length; j++) {
-      if (search2DArray(twoDimensionalArray, i, j, word)) {
-        count++;
-      }
+      count += search2DArray(twoDimensionalArray, i, j, word);
     }
   }
 
