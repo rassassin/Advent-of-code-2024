d026793262f851d451f7d7ee241c3b8ee624ded7
Reece Thandi <reecethandi11@gmail.com>
2025-01-01 18:37:32 +0000

    parsed data into a 2d array

--- day4/day4.js
+++ day4/day4.js
@@ -1,4 +1,11 @@
 const fs = require("fs");
-const input = fs.readFileSync("./day4input.txt", "utf-8").split("/\r?\n/");
+const input = fs.readFileSync("./day4input.txt", "utf-8").split("\n");
 
-console.log(input);
+const parseDataTo2DArray = (input) => input.map((row) => row.split(""));
+
+const solveDayFour = (input) => {
+  const twoDimensionalArray = parseDataTo2DArray(input);
+  return twoDimensionalArray;
+};
+
+console.log(solveDayFour(input));
