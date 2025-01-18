c270e760a7741ba8c67a2b2a22deeff4f90f47b9
Reece Thandi <reecethandi11@gmail.com>
2025-01-18 15:55:18 +0000

    uncommited changes

--- day4/day4.js
+++ day4/day4.js
@@ -3,8 +3,8 @@ const input = fs.readFileSync("./day4input.txt", "utf-8").split("\n");
 const xDir = [-1, -1, -1, 0, 0, 1, 1, 1];
 const yDir = [-1, 0, 1, -1, 1, -1, 0, 1];
 
-const xMasDir = [-1, -1, 1, 1];
-const yMasDir = [-1, 1, -1, 1];
+const xUpdatedDir = [-1, -1, 1, 1];
+const yUpdatedDir = [-1, 1, -1, 1];
 
 const parseDataTo2DArray = (input) => input.map((row) => row.split(""));
 
@@ -35,7 +35,7 @@ function searchForWord(twoDimensionalArray, word) {
   let count = 0;
 
   for (let i = 0; i < twoDimensionalArray.length; i++) {
-    for (let j = 0; j < twoDimensionalArray[0].length; j++) {
+    for (let j = 0; j < twoDimensionalArray[i].length; j++) {
       count += search2DArray(twoDimensionalArray, i, j, word);
     }
   }
@@ -43,13 +43,15 @@ function searchForWord(twoDimensionalArray, word) {
   return count;
 }
 
-const findMasPattern = (twoDimensionalArray, row, col, word) => {
-  if (twoDimensionalArray[row][col] !== word[0]) return false;
-
+const findMasPattern = (twoDimensionalArray, row, col) => {
   let count = 0;
 
-  for (let i = 0; i < twoDimensionalArray.length; i++) {
-    const name = "reece";
+  for (let dir = 0; dir < 4; dir++) {
+    let currX = row + xUpdatedDir[dir];
+    let currY = col + xUpdatedDir[dir];
+    for (let i = 0; i < twoDimensionalArray.length; i++) {
+      if (currX >= twoDimensionalArray.length || currX < 0 || currY >= twoDimensionalArray[i].length || currY < 0) break;
+    }
   }
 };
 
@@ -57,16 +59,17 @@ const searchForXmas = (twoDimensionalArray) => {
   let count = 0;
 
   for (let i = 0; i < twoDimensionalArray.length; i++) {
-    for (let j = 0; j < twoDimensionalArray[0].length; j++) {
-      count += findMasPattern(twoDimensionalArray, i, j);
+    for (let j = 0; j < twoDimensionalArray[i].length; j++) {
+      if (twoDimensionalArray[i][j] === "A") count += findMasPattern(twoDimensionalArray, i, j);
     }
   }
 };
 
 const solveDayFour = (input) => {
   const twoDimensionalArray = parseDataTo2DArray(input);
-  const findOccurencesOfXmas = searchForWord(twoDimensionalArray, "XMAS");
-  return findOccurencesOfXmas;
+  const partOne = searchForWord(twoDimensionalArray, "XMAS");
+  const partTwo = searchForXmas(twoDimensionalArray);
+  return { partOne, partTwo };
 };
 
 console.log(solveDayFour(input));
