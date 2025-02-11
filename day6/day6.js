d5ca5e0dba00fc0d87e7b555f0873f313073d167
Reece Thandi <reecethandi11@gmail.com>
2025-02-11 23:26:03 +0000

    started day 6

---
+++ day6/day6.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync("./day6input.txt", "utf-8").split("\n");
+
+const parseDataTo2DArray = (input) => input.map((row) => row.split(""));
+
+function findGuardPos(parsedInput) {
+  for (let y = 0; y < parsedInput.length; y++) {
+    for (let x = 0; x < parsedInput[y].length; x++) {
+      if (x === "^") return [x, y];
+    }
+  }
+}
+
+function solvePartOne(parsedInput) {
+  const [x, y] = findGuardPos(parsedInput);
+  console.log(x, y);
+}
+
+function solveDaySix(input) {
+  const parsedInput = parseDataTo2DArray(input);
+  const dayOne = solvePartOne(parsedInput);
+  return dayOne;
+}
+
+console.log(solveDaySix(input));
